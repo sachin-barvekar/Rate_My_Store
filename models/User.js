d2bdbd3a40149c874acf0d97a38359c4a492db63
sachin-barvekar <sachinbarvekar2003@gmail.com>
2025-04-03 17:18:21 +0530

    fix: add validation based on requirement for signup.

--- models/User.js
+++ models/User.js
@@ -1,10 +1,18 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true, minlength: 5, maxlength: 60 },
-  email: { type: String, required: true, unique: true },
+  name: { type: String, required: true, minlength: 20, maxlength: 60 },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+  },
   address: { type: String, maxlength: 400 },
-  password: { type: String, required: true },
+  password: {
+    type: String,
+    required: true,
+  },
   role: {
     type: String,
     enum: ['admin', 'normal', 'store_owner'],
